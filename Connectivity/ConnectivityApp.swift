6294acce169e04fe44064af2389c41d7d73d1a9c
giria <joan@giria.com>
2024-02-24 17:49:11 +0000

    Initial Commit

---
+++ Connectivity/ConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectivityApp.swift
+//  Connectivity
+//
+//  Created by joan Barrull on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
